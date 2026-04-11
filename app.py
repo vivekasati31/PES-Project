9cc7d02459e272072d58be60123a383a168ce689
VIVEK ASATI <92620259+vivekasati31@users.noreply.github.com>
2026-04-11 19:59:54 +0530

    n

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ uploaded_file = st.file_uploader("Upload image (.jpg, .jpeg, .png)", type=["jpg"
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
 
-    st.image(image, caption="Uploaded Image", use_container_width=True)
+    st.image(image, caption="Uploaded Image", use_column_width=True)
 
     if st.button("Predict", type="primary", disabled=(not model_ready or predictor is None)):
         try:
